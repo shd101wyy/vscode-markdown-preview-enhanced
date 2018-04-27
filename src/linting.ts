073890bf2ba3df4a44e7e374b3b42ceca7cdeda1
Alexander Kachkaev <alexander@kachkaev.ru>
2018-04-27 16:36:12 +0100

    Implement linting eporting via Diagnostics API

--- src/linting.ts
+++ src/linting.ts
@@ -1,8 +1,42 @@
 // @ts-ignore
 import { VFile, VFileBase } from "vfile";
-import * as reporter from "vfile-reporter";
+import * as vscode from "vscode";
+
+const diagnosticCollection = vscode.languages.createDiagnosticCollection();
 
 export const updateLintingReport = (vFiles: Array<VFile<{}>> = []) => {
-  // tslint:disable-next-line:no-console
-  console.log(reporter(vFiles));
+  diagnosticCollection.clear();
+
+  vFiles.forEach((vFile) => {
+    const diagnostics: vscode.Diagnostic[] = [];
+    vFile.messages.forEach((message) => {
+      const severity = {
+        true: vscode.DiagnosticSeverity.Error,
+        false: vscode.DiagnosticSeverity.Warning,
+        null: vscode.DiagnosticSeverity.Information,
+        undefined: vscode.DiagnosticSeverity.Information,
+      }[message.fatal as any];
+
+      const range = new vscode.Range(
+        Math.max(message.location.start.line - 1, 0),
+        Math.max(message.location.start.column - 1, 0),
+        Math.max(message.location.end.line - 1, 0),
+        Math.max(message.location.end.column - 1, 0),
+      );
+
+      const labels = [message.source, message.ruleId].filter(Boolean);
+      if (labels[0] && labels[0] === labels[1]) {
+        labels.pop();
+      }
+      let excerpt = message.reason.replace(/“([^”]+)”/g, "`$1`");
+      if (labels.length) {
+        excerpt += ` (${labels.join(":")})`;
+      }
+
+      const diagnostic = new vscode.Diagnostic(range, excerpt, severity);
+      diagnostic.source = "litvis";
+      diagnostics.push(diagnostic);
+    });
+    diagnosticCollection.set(vscode.Uri.file(vFile.path), diagnostics);
+  });
 };
