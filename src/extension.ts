facf35e32d022e5aaaa5733ae8d6ac244cf2bb08
Yiyi Wang <shd101wyy@gmail.com>
2022-05-12 20:26:08 +0800

    refactor: Removed console.log

--- src/extension.ts
+++ src/extension.ts
@@ -370,8 +370,6 @@ export function activate(context: vscode.ExtensionContext) {
 
   function clickTagA(uri, href) {
     href = decodeURIComponent(href);
-    // tslint:disable-next-line:no-console
-    console.log(href);
     href = href
       .replace(/^vscode\-resource:\/\//, "")
       .replace(/^vscode\-webview\-resource:\/\/(.+?)\//, "")
