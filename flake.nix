f94ad22e7ee5c3782c4620a6e892721b46dba40b
Yiyi Wang <shd101wyy@gmail.com>
2024-09-07 22:11:13 +0800

    Update crossnote to version 0.9.10 (#2041)
    * chore: Update crossnote to version 0.9.10
    
    * chore: Update nixpkgs to version nixos-24.05
    
    * chore: Ignore package.json in prettier formatting

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-23.11";
+      url = "github:NixOS/nixpkgs/nixos-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils = { url = "github:numtide/flake-utils"; };
