ba0e1fdd26b22b3dc2d217e1ac6246685a28b3dc
shd101wyy <shd101wyy@sina.com>
2025-03-16 21:48:10 +0800

    fix: Fixed the build for vscode web

--- flake.nix
+++ flake.nix
@@ -3,15 +3,36 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-24.05";
+      url = "github:NixOS/nixpkgs/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    unstableNixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in { devShell = import ./shell.nix { inherit pkgs; }; });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      unstableNixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        unstablePkgs = import unstableNixpkgs { inherit system; };
+      in
+      {
+        devShell = import ./shell.nix {
+          inherit pkgs;
+          inherit unstablePkgs;
+        };
+      }
+    );
 }
-
